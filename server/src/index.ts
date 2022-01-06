18077186df3d728cc5f69a87111917b1c5cc801f
Tom <thomas.m.heaton@gmail.com>
2022-01-06 17:37:06 +0000

    add fastify server code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,40 @@
 // Server
+
+import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
+import { Server, IncomingMessage, ServerResponse } from 'http'
+
+const server: FastifyInstance = Fastify({})
+
+const opts: RouteShorthandOptions = {
+    schema: {
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    pong: {
+                        type: 'string'
+                    }
+                }
+            }
+        }
+    }
+}
+
+server.get('/ping', opts, async (request, reply) => {
+    return { pong: 'it worked!' }
+})
+
+const start = async () => {
+    try {
+        await server.listen(3300)
+
+        const address = server.server.address()
+        const port = typeof address === 'string' ? address : address?.port
+
+    } catch (err) {
+        server.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
